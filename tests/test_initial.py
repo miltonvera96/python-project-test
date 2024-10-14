a0ba8f5bc6c6d6d53b6b48d822082c11f3834978
Milton Vera <miltonvera96@gmail.com>
2024-10-14 17:31:40 -0300

    Update test_initial.py

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -3,3 +3,10 @@ def test_sucess():
     Dummy test to check if the test suite is working
     """
     assert True
+
+
+def test_error():
+    """
+    Dummy test to check if the test suite is working
+    """
+    assert False
